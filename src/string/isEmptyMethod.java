f4cd30170ad83cfe58b4166860c37ba3aca5ff7a
Aishwarya samal <aishwaryasamal1111@gmail.com>
2022-08-23 17:25:07 +0530

    isEmptyMethod

---
+++ src/string/isEmptyMethod.java
@@ -0,0 +1,19 @@
+package string;
+
+public class isEmptyMethod {
+    public static void main(String[] args) {
+        String name ="";              // output name is empty
+
+        //String name = "abc";            // output valid name
+
+        if (name.isEmpty())
+
+        {
+            System.out.println("name is empty");
+        }
+
+        else{
+            System.out.println("valid name");
+        }
+    }
+}
